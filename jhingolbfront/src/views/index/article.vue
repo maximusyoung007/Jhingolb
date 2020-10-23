67d9466b18526fb7bb86e76f7119a1424d840d59
maximus <1091261998@qq.com>
2020-10-23 17:36:07 +0800

    文章列表

--- jhingolbfront/src/views/index/article.vue
+++ jhingolbfront/src/views/index/article.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
+  <div class="block">
+    <el-timeline>
+      <el-timeline-item timestamp="2018/4/12" placement="top">
+        <ul>
+          <li>更新 Github 模板</li>
+          <li>王小虎 提交于 2018/4/12 20:46</li>
+        </ul>
 
+
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/3" placement="top">
+        <ul>
+          <li>更新 Github 模板</li>
+          <li>王小虎 提交于 2018/4/12 20:46</li>
+        </ul>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/2" placement="top">
+        <ul>
+          <li>更新 Github 模板</li>
+          <li>王小虎 提交于 2018/4/12 20:46</li>
+        </ul>
+      </el-timeline-item>
+    </el-timeline>
   </div>
 </template>
 
