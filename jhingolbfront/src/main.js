6d933c173aa18c70ec3de7877450496aa5c4123b
maximus <1091261998@qq.com>
2020-10-29 11:37:20 +0800

    部署配置

--- jhingolbfront/src/main.js
+++ jhingolbfront/src/main.js
@@ -9,7 +9,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import 'ant-design-vue/dist/antd.css'
 
 var axios = require('axios');
-axios.defaults.baseURL = "http://localhost:8090";
+//axios.defaults.baseURL = "http://106.14.44.210:8090";
+axios.defaults.baseURL = "http://127.0.0.1:8090"
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
